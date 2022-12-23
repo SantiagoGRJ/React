f223e084c3b8c71bc942a3d1cbef89b620894ba4
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2022-12-23 12:05:33 -0600

    g

--- primer-app/src/App.js
+++ primer-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Primero from './components/primero';
 
 function App() {
   return (
     <div className="App">
-     
+     <Primero/>
     </div>
   );
 }
--- primer-app/src/components/primero.jsx
+++ primer-app/src/components/primero.jsx
@@ -1,6 +1,12 @@
-
+import React from "react";
 function Primero(){
     return(
-        
+        <div className="todo">
+            <img src="" alt="" />
+            <div className="testimonio">
+                <p>mi nombre es</p>
+            </div>
+        </div>
     );
 }
+export default Primero
